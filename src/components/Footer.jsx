efba32d49df7272271ccaf7a4a52ecf0dd824e3b
Arev87 <arevik@developandmarket.com>
2026-06-03 15:37:46 +0400

    space edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ function Footer({ onOpenBrochure }) {
         <a href="/fee_disclosure_notice.pdf" target="_blank">FARE Act</a>
     </div>
     <p className="center">
-        All information furnished herein is from sources deemed reliable. No representation is made by MNS nor is any to be implied as to the accuracy thereof and all information is submitted subject to errors, omissions, change of price, prior sale or lease, or withdrawal without notice. All dimensions are approximate. For exact dimensions, please hire your own architect or engineer. MNS, Licensed Real Estate Broker. 40 N 6th St, Brooklyn, NY 1124
+        All information furnished here in is from sources deemed reliable. No representation is made by MNS nor is any to be implied as to the accuracy thereof and all information is submitted subject to errors, omissions, change of price, prior sale or lease, or withdrawal without notice. All dimensions are approximate. For exact dimensions, please hire your own architect or engineer. MNS, Licensed Real Estate Broker. 40 N 6th St, Brooklyn, NY 1124
     </p>
 </footer>
   )
